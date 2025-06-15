0fb42ce9a44f893322343f68ec634db977eaec9b
malakistanifaha <rozimuhammadasqarov429@gmail.com>
2025-06-15 17:05:51 +0500

    Update portalkop.py

--- portalkop.py
+++ portalkop.py
@@ -73,7 +73,7 @@ if machine_code in hash_values_list:
         giv_ids_ozim = [row for row in reader if row]
         
     file_path_1 = r"C:\join\proxyglob.csv"
-    file_path_2 = r"/storage/emulated/0/giv/proxyglob.csv"
+    file_path_2 = r"/storage/emulated/0/giv/proxy.csv"
 
     if os.path.exists(file_path_1):
         with open(file_path_1, 'r') as f:
